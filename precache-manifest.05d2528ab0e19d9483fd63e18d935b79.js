ed3286b72a5ecfa630cf62c79e6566e6ef0bfa77
Rashi <rashikumari617@gmail.com>
2023-12-21 03:08:12 +0530

    Updates

---
+++ precache-manifest.05d2528ab0e19d9483fd63e18d935b79.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5c9208b6236a42a6127d0ba1abb9e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fd5abb0ddecbe270a37",
+    "url": "/static/css/main.70bd2d5d.chunk.css"
+  },
+  {
+    "revision": "234651d6ea393c34c747",
+    "url": "/static/js/2.16aed5e1.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/static/js/2.16aed5e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fd5abb0ddecbe270a37",
+    "url": "/static/js/main.a81a8b67.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "f9bc8b6ce69388ab5ce2eeda178c64bf",
+    "url": "/static/media/136920.f9bc8b6c.jpg"
+  },
+  {
+    "revision": "230190f115be8aaaea6119cc7d2bf310",
+    "url": "/static/media/f.230190f1.mp4"
+  },
+  {
+    "revision": "bfb7a8c97a494dc198b4ace0965a7205",
+    "url": "/static/media/logo.bfb7a8c9.jpg"
+  }
+]);
